247ef8b1e25b73ea0daa6fc630ce01b002df8cc1
Sourin Mukherjee <sourin.mukherjee2580@gmail.com>
2024-05-28 21:21:40 +0530

    added new

--- largest.java
+++ largest.java
@@ -1,10 +1,16 @@
-import java.util.*;
- 
-public class largest
-{
-    public static void main (String args[])
-    {
-        Scanner sc = new Scanner (System.in);
-        
+import java.util.* ;
+import java.io.*; 
+
+public class largest {
+
+    static int largestElement(int[] arr, int n) {
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<n;i++)
+        {
+            max = Math.max(max,arr[i]);
+        }
+
+        return max;
+
     }
 }
---
+++ second_largest.java
@@ -0,0 +1,10 @@
+import java.util.*;
+ 
+public class second_largest
+{
+    public static void main (String args[])
+    {
+        Scanner sc = new Scanner (System.in);
+        
+    }
+}
